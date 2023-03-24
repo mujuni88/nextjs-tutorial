5e545c89b5dee3be7dded462ac82f52361c4f2d7
Joe Buza <jbuza@netflix.com>
2023-03-24 16:45:16 -0700

    chore: remove console.log

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ export default async function Home() {
   const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`;
 
   const data = await (await fetch(url)).json();
-  console.log(data)
   return (
     <main className='p-2'>
       <h1 className="text-lg text-lime-300">Popular Movies</h1>
