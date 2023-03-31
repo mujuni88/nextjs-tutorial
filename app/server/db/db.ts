a4f875eaba3bf9b012d527f4b50ad664bad488bf
Joe Buza <jbuza@netflix.com>
2023-03-31 16:40:42 -0700

    chore: fix env variables

--- app/server/db/db.ts
+++ app/server/db/db.ts
@@ -1,12 +1,11 @@
-
 import { drizzle } from 'drizzle-orm/planetscale-serverless';
 import { connect } from '@planetscale/database';
-import { env } from '@/env.mjs'
+import { env } from '@/env.mjs';
 
 const connection = connect({
-  host: env['DB_HOST'],
-  username: env['DB_USERNAME'],
-  password: env['DB_PASSWORD'],
+  host: env['PLANETSCALE_DB_HOST'],
+  username: env['PLANETSCALE_DB_USERNAME'],
+  password: env['PLANETSCALE_DB_PASSWORD'],
 });
 
 export const db = drizzle(connection);
